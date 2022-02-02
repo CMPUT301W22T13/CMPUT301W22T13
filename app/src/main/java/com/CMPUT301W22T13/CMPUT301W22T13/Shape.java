bdced3f8fe2543d0e9b1c00929e2da7dde9e3732
bdirksen <bdirksen@ualberta.ca>
2022-02-02 16:55:40 -0700

    Update Shape.java
    added a color to shape

--- app/src/main/java/com/CMPUT301W22T13/CMPUT301W22T13/Shape.java
+++ app/src/main/java/com/CMPUT301W22T13/CMPUT301W22T13/Shape.java
@@ -1,14 +1,16 @@
 package com.CMPUT301W22T13.CMPUT301W22T13;
 
 public abstract class Shape {
-
     protected int x;
     protected int y;
+    protected String color = "black";
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+
     }
+
 
     public int getX() {
         return x;
