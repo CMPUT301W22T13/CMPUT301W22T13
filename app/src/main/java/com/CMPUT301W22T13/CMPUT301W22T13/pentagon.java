a1c020482216c44a6955fa004095e5f20dacb7ce
bdirksen <bdirksen@ualberta.ca>
2022-02-02 16:13:12 -0700

    Create pentagon.java

---
+++ app/src/main/java/com/CMPUT301W22T13/CMPUT301W22T13/pentagon.java
@@ -0,0 +1,28 @@
+package com.CMPUT301W22T13.CMPUT301W22T13;
+
+public class pentagon extends Shape{
+    private int vertices;
+    private int sides;
+
+    public pentagon(int x, int y) {
+        super(x, y);
+        this.vertices = vertices;
+        this.sides = sides;
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+
+    public void setVertices(int vertices) {
+        this.vertices = vertices;
+    }
+
+    public int getSides() {
+        return sides;
+    }
+
+    public void setSides(int sides) {
+        this.sides = sides;
+    }
+}
