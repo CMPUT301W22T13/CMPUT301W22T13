ec2236e31a40f850175875192a7a214f5072a2ed
Rajan Maghera <rmaghera@ualberta.ca>
2022-02-02 16:00:10 -0700

    Created abstract shape class
    Co-Authored-By: Youngwoo Seo <63736151+kakaname@users.noreply.github.com>
    Co-Authored-By: elskhn <21252681+elskhn@users.noreply.github.com>
    Co-Authored-By: Smehjabe <77705366+Smehjabe@users.noreply.github.com>
    Co-Authored-By: Ilia Kamyab <97188350+IliaKamyab@users.noreply.github.com>
    Co-Authored-By: bdirksen <97187485+bdirksen@users.noreply.github.com>

---
+++ app/src/main/java/com/CMPUT301W22T13/CMPUT301W22T13/Shape.java
@@ -0,0 +1,28 @@
+package com.CMPUT301W22T13.CMPUT301W22T13;
+
+public abstract class Shape {
+
+    protected int x;
+    protected int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
