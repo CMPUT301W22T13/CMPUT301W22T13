62f195ee859c84576f8de7731b7c73d5784a2afb
Ilia Kamyab <kamyabfa@ualberta.ca>
2022-02-02 16:35:00 -0700

    added Triangle

---
+++ app/src/main/java/com/CMPUT301W22T13/CMPUT301W22T13/Triangle.java
@@ -0,0 +1,14 @@
+package com.CMPUT301W22T13.CMPUT301W22T13;
+
+public class Triangle extends Shape{
+
+    private Integer area;
+
+    public Integer getLength(){
+        return area;
+    }
+    public Triangle(int x, int y) {
+        super(x, y);
+    }
+
+}
