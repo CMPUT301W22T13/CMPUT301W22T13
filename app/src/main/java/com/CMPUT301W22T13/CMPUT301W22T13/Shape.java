9fcef4c11775a8d70eb3ee6287641c5831f19c40
Ilia Kamyab <kamyabfa@ualberta.ca>
2022-02-02 17:08:35 -0700

    added aquamarine color to shape

--- app/src/main/java/com/CMPUT301W22T13/CMPUT301W22T13/Shape.java
+++ app/src/main/java/com/CMPUT301W22T13/CMPUT301W22T13/Shape.java
@@ -5,7 +5,7 @@ public abstract class Shape {
     protected int x;
     protected int y;
 
-    String color = "Magenta";
+    String color = "aquamarine";
 
     public Shape(int x, int y) {
         this.x = x;
